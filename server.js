3e27d690501a2dccced8c35da61f39e93848c8c6
toastynerd <tylermorgan86@gmail.com>
2015-08-03 12:58:30 -0700

    start of day one

---
+++ server.js
@@ -0,0 +1,17 @@
+'use strict';
+
+var mongoose = require('mongoose');
+var express = require('express');
+var app = express();
+
+var notesRoutes = express.Router();
+
+mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost/notes_development');
+
+require('./routes/notes_routes')(notesRoutes);
+
+app.use('/api', notesRoutes);
+
+app.listen(process.env.PORT || 3000, function() {
+  console.log('server running on port ' + (process.env.PORT || 3000));
+});
