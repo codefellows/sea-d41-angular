a2c6c60c0ee67ee063d5845574329b852af1b884
toastynerd <tylermorgan86@gmail.com>
2015-08-06 15:54:21 -0700

    testing

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@
 
 var gulp = require('gulp');
 var webpack = require('gulp-webpack');
+var KarmaServer = require('karma').Server;
 
 gulp.task('webpack:dev', function() {
   return gulp.src('app/js/client.js')
@@ -13,10 +14,27 @@ gulp.task('webpack:dev', function() {
     .pipe(gulp.dest('build/'));
 });
 
+gulp.task('webpack:test', function() {
+  return gulp.src('test/karma_tests/entry.js')
+    .pipe(webpack({
+      output: {
+        filename: 'test_bundle.js'
+      }
+    }))
+    .pipe(gulp.dest('test/karma_tests/'));
+});
+
 gulp.task('copy', function() {
   return gulp.src('app/**/*.html')
     .pipe(gulp.dest('build/'));
 });
 
+gulp.task('karmatest', ['webpack:test'], function(done) {
+  new KarmaServer({
+    configFile: __dirname + '/karma.conf.js'
+  }, done).start(); 
+});
+
 gulp.task('build', ['webpack:dev', 'copy']);
-gulp.task('default', ['build']);
+gulp.task('default', ['karmatest', 'build']);
+
---
+++ test/karma_tests/entry.js
@@ -0,0 +1,1 @@
+require(__dirname + '/notes_controller_tests');
